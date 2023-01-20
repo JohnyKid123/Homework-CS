babcd0d435c5a0765176716e325304507df84a6d
Alex Venkov <97468479+JohnyKid123@users.noreply.github.com>
2023-01-20 10:41:29 +0200

    Update program.cs

--- SIT/Functional Programming/ex.2/program.cs
+++ SIT/Functional Programming/ex.2/program.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
 
-int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
-int count = numbers.Count();
-Console.WriteLine(count);
-int sum = numbers.Sum();
-Console.WriteLine(sum);
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int count = numbers.Count();
+        Console.WriteLine(count);
+        int sum = numbers.Sum();
+        Console.WriteLine(sum);
+    }
+}
